c127c0cc474e7f730a9e5767f38c6de877ad92bf
sam-bansal <sam.bansal.2002@gmail.com>
2021-07-31 21:58:14 +0530

    Add project files.

---
+++ Employee DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Employee_DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public IEnumerable<employee> Get()
+        {
+            using(happyEntities entities = new happyEntities())
+            {
+                 return entities.employee.ToString;
+            }
+        }
+
+        public employee Get(int id)
+        {
+            using (happyEntities entities = new happyEntities())
+            {
+                return entities.employee.FirstOrdefault(e => e.ID == id);
+            }
+        }
+    }
+}
